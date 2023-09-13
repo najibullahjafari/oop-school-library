4083dc896f2411b90f052a4219bbee879d1f2a1b
Najibullah Jafari <121656832+najibullahjafari@users.noreply.github.com>
2023-09-14 04:13:23 +0430

    unmerge

--- teacher.rb
+++ teacher.rb
@@ -10,3 +10,4 @@ class Teacher < Person
     true
   end
 end
+
--- trimdecorator.rb
+++
@@ -1,8 +0,0 @@
-require './decoratorbase'
-# For the TrimmerDecorator: Create a class that inherits from the base Decorator class.
-
-class TrimmerDecorator < DecoratorBase
-  def correct_name
-    super[0, 10]
-  end
-end
