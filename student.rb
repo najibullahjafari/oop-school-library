adb0c4ab668f7fb2dd108018a1973efe5c1efa86
Najibullah Jafari <121656832+najibullahjafari@users.noreply.github.com>
2023-09-14 09:01:38 +0430

    solve linter

--- student.rb
+++ student.rb
@@ -10,9 +10,3 @@ class Student < Person
     '¯(ツ)/¯'
   end
 end
-person = Student.new(22, 'maximilianus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
--- teacher.rb
+++ teacher.rb
@@ -1,4 +1,5 @@
 require './person'
+
 class Teacher < Person
   def initialize(id, specialization, name = 'Unknown', age = 0, parent_permission: true)
     super(id, name, age, parent_permission)
