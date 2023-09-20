faaf867400a0dad936aeeccb676bef35acc936f4
MasumaJaffery <masumajaffery1@gmail.com>
2023-09-20 11:01:35 -0700

    Rectify-2

--- book.rb
+++ book.rb
@@ -1,13 +1,22 @@
 class Book
-  attr_accessor :title, :author, :rentals
+  attr_accessor :title, :author, :id, :rentals
+
+  @@last_id = 0
 
   def initialize(title, author)
     @title = title
     @author = author
+    @id = generate_id
     @rentals = []
   end
 
   def add_rental(person, date)
     @rentals << Rental.new(date, self, person)
   end
+
+  def generate_id
+    @@last_id += 1
+  end
+
+  # Add methods for handling rentals, e.g., check_in and check_out
 end
