00962ef64a84efb2b38257ddd6b54f01ec0345f2
MasumaJaffery <masumajaffery1@gmail.com>
2023-09-21 11:32:41 -0700

    Teacher Test

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -3,7 +3,7 @@ require_relative '../classroom'
 describe Classroom do
   let(:classroom_label) { 'Microverse' }
   let(:student) { double('Student') }
-  
+
   subject { described_class.new(classroom_label) }
 
   it 'should initialize with the correct label' do
@@ -13,5 +13,4 @@ describe Classroom do
   it 'should start with an empty list of students' do
     expect(subject.students).to be_empty
   end
-
 end
--- spec/nameabe_spec.rb
+++ spec/nameabe_spec.rb
@@ -4,6 +4,8 @@ describe Nameable do
   subject { described_class.new }
 
   it 'should raise NotImplementedError for correct_name' do
-    expect { subject.correct_name }.to raise_error(NotImplementedError, 'Subclasses must implement the correct_name method')
+    expect do
+      subject.correct_name
+    end.to raise_error(NotImplementedError, 'Subclasses must implement the correct_name method')
   end
 end
