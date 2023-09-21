6fc53da51a8d020d4e10bfdf6e6ff4e6abbf7c95
Najibullah Jafari <121656832+najibullahjafari@users.noreply.github.com>
2023-09-21 16:52:01 +0430

    Add student test

---
+++ spec/student_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../student'
+
+describe Student do
+    let(:classroom) { double('Classroom', students: []) }
+    context "test student class" do
+        it "this will test to output of student class! " do
+        s = Student.new("Najib", 22, classroom)
+        name = s.name
+        age = s.age
+        classroom = s.classroom
+        expect(name).to eq("Najib")
+        expect(age).to eq(22)
+        expect(classroom).to eq(classroom)
+        end
+    end
+end
