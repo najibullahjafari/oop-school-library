8315c3a10f18013f6b06be0755b4dfa9bfad3e79
Najibullah Jafari <121656832+najibullahjafari@users.noreply.github.com>
2023-09-22 03:44:49 +0430

    Update person.rb

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -9,5 +9,29 @@ describe Person do
       expect(age).to eq(22)
       expect(name).to eq('Najib')
     end
+
+    it 'Should test if a person can use services?' do
+      person = Person.new('Najib', 22, parent_permission: true)
+      expect(person.can_use_services?).to eq(true)
+    end
+
+    it 'Should test if a person can use services? under age of 18' do
+      person = Person.new('Najib', 12, parent_permission: false)
+      expect(person.can_use_services?).to eq(false)
+    end
+
+    it 'should test if the name of a person is trimmed?' do
+      person = Person.new('Najib', 22, parent_permission: true)
+      correctname = person.correct_name
+      expect(correctname.length <= 10).to be true
+    end
+
+    it "should add a rental to a person's rentals list" do
+      person = Person.new('Najib', 22, parent_permission: true)
+      book = Book.new('Book Title', 'Author')
+      date = Date.new(2023, 9, 30)
+      person.add_rental(book, date)
+      expect(person.rentals).to include(an_instance_of(Rental))
+    end
   end
 end
